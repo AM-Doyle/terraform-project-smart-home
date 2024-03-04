0c9c6d396e0c3547325234d2d8649dbaac4f1dc2
AM-Doyle <alexmikedoyle@gmail.com>
2024-03-04 14:52:34 +0000

    completed the security group module

---
+++ body/modules/security/mainSecurity.tf
@@ -0,0 +1,89 @@
+data "http" "myipaddr" {
+   url = "http://icanhazip.com"
+}
+
+resource "aws_security_group" "tp-sg" {
+  name        = "tp-sg"
+  description = "Security group for app servers"
+  vpc_id      = var.vpc_id
+}
+
+resource "aws_security_group_rule" "allow_http_in" {
+  type              = "ingress"
+  from_port         = 80
+  to_port           = 80
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]
+  ipv6_cidr_blocks  = ["::/0"]
+  security_group_id = aws_security_group.tp-sg.id
+}
+resource "aws_security_group_rule" "allow_http_out" {
+  type              = "egress"
+  from_port         = 80
+  to_port           = 80
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]
+  ipv6_cidr_blocks  = ["::/0"]
+  security_group_id = aws_security_group.tp-sg.id
+}
+
+resource "aws_security_group_rule" "allow_https_in" {
+  type              = "ingress"
+  from_port         = 443
+  to_port           = 443
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]
+  ipv6_cidr_blocks  = ["::/0"]
+  security_group_id = aws_security_group.tp-sg.id
+}
+resource "aws_security_group_rule" "allow_https_out" {
+  type              = "egress"
+  from_port         = 443
+  to_port           = 443
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]
+  ipv6_cidr_blocks  = ["::/0"]
+  security_group_id = aws_security_group.tp-sg.id
+}
+
+resource "aws_security_group_rule" "allow_3000_in" {
+  type              = "ingress"
+  from_port         = 3000
+  to_port           = 3000
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]
+  ipv6_cidr_blocks  = ["::/0"]
+  security_group_id = aws_security_group.tp-sg.id
+}
+
+resource "aws_vpc_security_group_ingress_rule" "ssh" {
+  security_group_id = aws_security_group.tp-sg.id
+
+  cidr_ipv4   = "${chomp(data.http.myipaddr.response_body)}/32"
+  from_port   = 22
+  ip_protocol = "tcp"
+  to_port     = 22
+}
+
+resource "aws_vpc_security_group_ingress_rule" "ssh_local" {
+  security_group_id = aws_security_group.tp-sg.id
+
+  cidr_ipv4   = var.cidr_block
+  from_port   = 22
+  ip_protocol = "tcp"
+  to_port     = 22
+}
+
+resource "aws_vpc_security_group_egress_rule" "outgoing_ipv6" {
+  security_group_id = aws_security_group.tp-sg.id
+
+  cidr_ipv6   = "::/0"
+  ip_protocol = "-1"
+}
+
+resource "aws_vpc_security_group_egress_rule" "outgoing_ipv4" {
+  security_group_id = aws_security_group.tp-sg.id
+
+  cidr_ipv4   = "0.0.0.0/0"
+  ip_protocol = "-1"
+}
